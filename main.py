4472e579dc57064b7a20478fb0998811a8bc1b49
tuf84490 <46969584+tuf84490@users.noreply.github.com>
2019-04-09 11:38:37 -0400

    initial setup

---
+++ main.py
@@ -0,0 +1,119 @@
+import sys
+import subprocess
+import os
+import sqlite3
+import datetime
+
+def openConfig():
+    #search through the current directory for a specified string. Get input from config file and repeat for each string found in config file.
+    searchStrings = []
+    configFile = open("config.txt")
+    configLine = configFile.readline()
+    while (configLine != ''):
+        aString = configLine.rstrip()
+        searchStrings.append(aString)
+        configLine = configFile.readline()
+    configFile.close()
+    return searchStrings
+
+#pass in a pointer to a log file that we write to whenever we find a match
+def search(listOfStrings, packageLocation, output):
+    path = packageLocation + "/"
+    for file in os.listdir(path):
+        fileIndex = path + file
+        print("NOW SCANNING : " + fileIndex)
+        #check to see if its a file first, because if its a directory then we cant really open and scan through it
+        if(os.path.isfile(fileIndex)):
+            openFile = open(fileIndex)
+            try:
+                line = openFile.readline()
+            except:
+                #if we are unable to read the lines in the file due to a strange encoding, just skip this file
+                break
+            lineNumber = 1
+            while (line != ''):
+                #search through every line for all search terms
+                for string in listOfStrings:
+                    #make the search terms not case sensitive to reduce number of searches performed
+                    caseInsensitiveLine = line.lower()
+                    currentString = caseInsensitiveLine.find(string.lower())
+                    if(currentString != -1):
+                        #instead of printing these, eventually log them by getting them all into a huge log string and storing it in the database
+                        output.write("FOR TERM " + string + ", MATCH IN: " + fileIndex + ", AT LINE: " + str(lineNumber) + ", LINE : " + line)
+                #always encompas the readline in a try catch block in case encoding errrors prevent us from reading a specific line
+                try:
+                    line = openFile.readline()
+                except:
+                    break                
+                lineNumber += 1
+            openFile.close()
+        else:
+            for string in listOfStrings:
+                caseInsensitiveIndex = fileIndex.lower()
+                #if the directory itself contains a suspicious name, log it
+                if(caseInsensitiveIndex.find(string.lower()) != -1):
+                    #instead of printing these, eventually log them by getting them all into a huge log string and storing it in the database
+                    output.write("FOR TERM " + string + ", SUSPICIOUS DIRECTORY FOUND AT: " + fileIndex)
+                #if its is a directory, search through it recursively
+                if(os.path.exists(fileIndex)):
+                    search(listOfStrings, fileIndex, output)
+
+
+#get input cleanly. Will do both by default (mode = 0). If mode = 1 just download, if mode = 2 just unpackage/just scan
+inputs = len(sys.argv)
+if(inputs == 2):
+    link = sys.argv[1]
+    mode = 0
+elif(inputs == 3):
+    link = sys.argv[1]
+    mode = int(sys.argv[2])
+else:
+    print("ERROR. Invalid input. Please try again.")
+    sys.exit()
+if(mode > 2 or mode < 0):
+    print("ERROR. Mode can only be 1 for just download, 2 for just unpackage, or 0 for both (is by default, do not need to specify 0). Please try again.")
+    sys.exit()
+
+database = sqlite3.connect("appStats.db")
+dbManager = database.cursor()
+command = """CREATE TABLE IF NOT EXISTS stats (
+                app_name VARCHAR(255),
+                date_checked DATETIME,
+                dangerous bit,
+                output_log MEDIUMTEXT
+            );"""
+dbManager.execute(command)
+
+download = "gplaycli --device-codename hammerhead -d " + link
+threshold = 10
+if(mode == 1 or mode == 0):
+    print("Now downloading " + link)
+    while threshold > 0:
+        errorcheck = subprocess.run(download, shell=True, capture_output=True)
+        #if stderr is an empty byte (aka b''), that means it downloaded so continue
+        if(errorcheck.stderr == b''):
+            print("Finished Download.")
+            break
+        #if not keep trying 
+        else:
+            errorString = errorcheck.stderr.decode("utf-8")
+            print("Failed because : " + errorString + ". Retrying .... ")
+            threshold -= 1
+    if(threshold <= 0):
+        print("Could not download the apk. Please try again.")
+        sys.exit()
+
+apkName = link + ".apk"
+unpackage = "apktool d " + apkName
+if(mode == 2 or mode == 0):
+    print("Now unpackaging " + apkName)
+    subprocess.run(unpackage, shell=True)
+
+#open the output file
+outputFileName = apkName + "Output.txt"
+output = open(outputFileName, "w")
+#get all words to search for
+configResults = openConfig()
+#search through the package for suspicious terms
+search(configResults, link, output)
+output.close()
